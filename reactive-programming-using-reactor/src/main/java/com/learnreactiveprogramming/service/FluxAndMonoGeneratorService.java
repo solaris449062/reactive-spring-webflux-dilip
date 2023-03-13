a8a621a5bd7bd5a2d560d2afb334b994d32d8dda
solaris449062 <spark@spark.local>
2023-03-12 22:06:42 -0600

    Mono and Flux hands on

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,29 @@
 package com.learnreactiveprogramming.service;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
 public class FluxAndMonoGeneratorService {
+
+    public Flux<String> namesFlux() {
+        return Flux.fromIterable(List.of("Alex", "Ben", "Chloe")) // emulating db or a remote service call
+                .log();
+    }
+
+    public Mono<String>  nameMono() {
+        return Mono.just("Alex")
+                .log();
+    }
+
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+
+        fluxAndMonoGeneratorService.namesFlux()
+                .subscribe(name -> System.out.println("Flux:" + name));
+
+        fluxAndMonoGeneratorService.nameMono()
+                .subscribe(name -> System.out.println("Mono: " + name));
+    }
 }
